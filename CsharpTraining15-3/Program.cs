ce9eecc12ed6bb548f851812aa1b1ab26b38c339
pon541998 <pon541998@gmail.com>
2025-02-20 16:11:27 +0800

    20250220update

---
+++ CsharpTraining15-3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTraining15_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelpAttribute helpAttribute;
+
+            foreach(var attr in typeof(AnyClass).GetCustomAttributes(true) )
+            {
+                helpAttribute = attr as HelpAttribute;
+                if (null != helpAttribute)
+                {
+                    Console.WriteLine("AnyClass Description: {0}",
+                        helpAttribute.Description);
+                }
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class HelpAttribute : Attribute
+    {
+        protected string description;
+        public HelpAttribute(string description_in)
+        {
+            this.description = description_in;
+        }
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    [Help("this is a do-nothing class", Name = "Jikexueyuan")]
+    public class AnyClass
+    { }
+}
